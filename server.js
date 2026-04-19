d80020069793b6be54a83cc30648d60ce58fb583
businessmen1of1-oss <businessmen1of1@gmail.com>
2026-04-19 19:38:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
-
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
 let orders = [];
 
 app.get('/admin', (req, res) => {
